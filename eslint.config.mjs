1b4dd7a14fd3c6d0ab34df428682660253c0d9e9
Tolani Akintayo <devopsforjesus@gmail.com>
2026-07-16 12:03:05 +0000

    fix: pipeline issues in eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,11 +3,26 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], 
-    plugins: { js }, 
-    extends: ["js/recommended"], 
-    languageOptions: { 
-      globals: globals.node 
+  // Node.js application files
+  {
+    files: ["**/*.{js,cjs,mjs}", "!**/*.test.js"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+
+  // Jest test files
+  {
+    files: ["**/*.test.js"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
     },
   },
 ]);
